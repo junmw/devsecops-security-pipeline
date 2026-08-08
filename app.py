c4bb9d9893ef3c3ac754344d860cb3638ff071b1
Jun <junmw@users.noreply.github.com>
2026-08-07 23:53:34 -0400

    Update AWS secret key in process_user function
    Replaced leaked AWS secret key with a valid key.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def process_user():
     print("Initializing application...")
 
-    AWS_SECRET_KEY = "LEAKED123PASSWORD"
+    AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
         
     user_status = "Active"
     return user_status
