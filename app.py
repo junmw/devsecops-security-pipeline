1b0c898b592937f44f1951ea647cb5f9ad20c9ae
Jun <junmw@users.noreply.github.com>
2026-08-07 23:51:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def process_user():
     print("Initializing application...")
 
-    AWS_SECRET_KEY = "LEAKEDPASSWORD"
+    AWS_SECRET_KEY = "LEAKED123PASSWORD"
         
     user_status = "Active"
     return user_status
